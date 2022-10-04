41e651844bfc6aa5765fdd4791f9389c90f2b93c
Muhammed Busari Ridwan <bumuhammed@ecobank.com>
2022-10-04 09:20:09 +0100

    modified print prime numbers

--- src/main/java/com/busaridwan/amigoscode/dsa/PrimeNumber.java
+++ src/main/java/com/busaridwan/amigoscode/dsa/PrimeNumber.java
@@ -19,19 +19,27 @@ public class PrimeNumber {
         return check;
     }
     static int printPrime(int num){
-        for (int i = 2; i <= num; i++){
-            boolean isPrime = true;
-            for(int div = 2; div <= i/2; div++){
-                if (i % div == 0){
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime){
+        for (int i=0; i<=num; i++){
+            if(isPrime(i)){
                 System.out.println(i);
             }
-
         }
         return num;
     }
+//    static int printPrime(int num){
+//        for (int i = 2; i <= num; i++){
+//            boolean isPrime = true;
+//            for(int div = 2; div <= i/2; div++){
+//                if (i % div == 0){
+//                    isPrime = false;
+//                    break;
+//                }
+//            }
+//            if (isPrime){
+//                System.out.println(i);
+//            }
+//
+//        }
+//        return num;
+//    }
 }
