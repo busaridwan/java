2e628f1bb1675eb6c4857fc37112ed1c54535814
Muhammed Busari Ridwan <bumuhammed@ecobank.com>
2022-10-04 09:12:58 +0100

    Printing prime numbers

---
+++ src/main/java/com/busaridwan/amigoscode/dsa/PrimeNumber.java
@@ -0,0 +1,37 @@
+package com.busaridwan.amigoscode.dsa;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int x = 173;
+        System.out.println(isPrime(x));
+        printPrime(x);
+    }
+
+    static boolean isPrime(int num){
+        boolean check = true;
+        for (int div = 2; div <= num/2; div++){
+            if (num % div == 0){
+                check = false;
+                return check;
+            }
+        }
+        return check;
+    }
+    static int printPrime(int num){
+        for (int i = 2; i <= num; i++){
+            boolean isPrime = true;
+            for(int div = 2; div <= i/2; div++){
+                if (i % div == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println(i);
+            }
+
+        }
+        return num;
+    }
+}
